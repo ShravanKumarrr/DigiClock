1efb4505677ab7648dd2139a49fa7cc9da6c4499
ShravanKumarrr <87751107+ShravanKumarrr@users.noreply.github.com>
2021-07-21 12:57:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let clock = () => {
+    let date = new Date();
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+    let amorpm = "AM"
+
+    if (hrs == 0) {
+        hrs = 12
+    }
+    else if (hrs >= 12) {
+        amorpm = "PM"
+
+    }
+    else if (hrs > 12) {
+        hrs = hrs - 12
+    }
+
+    document.getElementById('hours').innerHTML = hrs
+    document.getElementById('minutes').innerHTML = mins
+    document.getElementById('secounds').innerHTML = secs
+    document.getElementById('amorpm').innerHTML = amorpm
+
+    setInterval(clock, 500)
+};
+
+clock();
